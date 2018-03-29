16a51320fdf86d4ee1eda754e64673358c5348f8
zhengyueguo <1163342393@qq.com>
2018-03-29 23:49:18 +0800

    测试push

--- src/main/java/com/pxc/SellApplication.java
+++ src/main/java/com/pxc/SellApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SellApplication {
-
+	/*测试提交*/
 	public static void main(String[] args) {
 		SpringApplication.run(SellApplication.class, args);
 	}
